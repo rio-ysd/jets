83566ccf41de6e0532c7b0e3db3872ca3fa15779
Rio Yoshida <JPZ6173@MN01582375.local>
2022-08-10 16:19:20 +0900

    Modified something

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Jets.application.routes.draw do
   resources :companies
   resources :designs
   namespace :mypage do
+    get 'analytics', to: 'analytics#index'
     resources :layouts
     resources :pages
   end
