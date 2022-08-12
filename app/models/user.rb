0c0e7c7afa96a4db13b6dcd8687d83461c0c254b
shohei <shohei.miyagi@devenues.com>
2022-08-12 17:48:40 +0900

    userのvalidates,required追加　designコントローラの指定IDをセレクトで選択したIDになるように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
   belongs_to :company
 
-  
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+
+  validates :name, uniqueness: true
+  validates :email, uniqueness: true
+
+
 end
