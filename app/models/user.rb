45f9d45b5746f62cb7aebd1b705844435acde69c
shohei <shohei.miyagi@devenues.com>
2022-08-12 18:26:50 +0900

    バリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,7 @@
 class User < ApplicationRecord
   belongs_to :company
 
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, presence: true
-
   validates :name, uniqueness: true
   validates :email, uniqueness: true
-
 
 end
